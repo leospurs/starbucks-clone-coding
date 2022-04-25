ca3d858fe3bfcc025619b511d9e2f8e9939108b3
Lee hojae <lehojae@gmail.com>
2022-04-25 23:43:04 +0900

    Feat : badge 추가

--- js/main.js
+++ js/main.js
@@ -16,3 +16,25 @@ searchInputEl.addEventListener('blur', function(){
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
 });
+
+// 화면 스크롤 시 실행되는 함수(lodash throttle 함수 사용)
+const badgeEl = document.querySelector('.badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+    console.log(window.scrollY);
+    if(window.scrollY > 500) {
+        // 배지 비노출
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        });
+    } else {
+        // 배지 노출
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        });
+    }
+}, 300));
+// _.throttle(함수, 시간(밀리세컨드 단위로));
+// gsap.to(요소, 지속시간, 옵션);
