f31661ab6747717cae710c478af79f925499ea08
Lee hojae <lehojae@gmail.com>
2022-05-02 19:09:17 +0900

    Fix : promotion 영역 숨김 js 추가

--- js/main.js
+++ js/main.js
@@ -71,3 +71,17 @@ new Swiper('.promotion .swiper-container', {
         nextEl: '.promotion .swiper-next'
     }
 });
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function (){
+    isHidePromotion = !isHidePromotion
+    if (isHidePromotion) {
+        // 비노출 처리
+        promotionEl.classList.add('hide');
+    } else {
+        // 노출 처리
+        promotionEl.classList.remove('hide');
+    }
+});
