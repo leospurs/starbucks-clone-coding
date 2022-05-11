2717f594c1494f2929b01bae489816d5ac9c137d
Lee hojae <lehojae@gmail.com>
2022-05-11 15:09:18 +0900

    FEAT : RESERVE, FAVORITE 영역 추가

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@ searchEl.addEventListener('click', function () {
 });
 
 // search영역의 input에 focus 시 실행되는 함수
-searchInputEl.addEventListener('focus', function(){
+searchInputEl.addEventListener('focus', function () {
     searchEl.classList.add('focused');
     searchInputEl.setAttribute('placeholder', '통합검색');
 });
 
 // search영역의 input에 focus 해제 시 실행되는 함수
-searchInputEl.addEventListener('blur', function(){
+searchInputEl.addEventListener('blur', function () {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
 });
@@ -22,7 +22,7 @@ const badgeEl = document.querySelector('.badges');
 
 window.addEventListener('scroll', _.throttle(function () {
     console.log(window.scrollY);
-    if(window.scrollY > 500) {
+    if (window.scrollY > 500) {
         // 배지 비노출
         gsap.to(badgeEl, .6, {
             opacity: 0,
@@ -41,10 +41,10 @@ window.addEventListener('scroll', _.throttle(function () {
 
 // 새로고침 시 visual 영역 이미지를 순차적으로 노출시키는 함수
 const fadeEls = document.querySelectorAll('.visual .fade-in');
-fadeEls.forEach(function(fadeEl, index) {
+fadeEls.forEach(function (fadeEl, index) {
     gsap.to(fadeEl, 1, {
-        delay: (index+1)*.7,
-        opacity: 1 
+        delay: (index + 1) * .7,
+        opacity: 1
     });
 });
 
@@ -55,9 +55,9 @@ new Swiper('.notice-line .swiper-container', {
     loop: true
 })
 new Swiper('.promotion .swiper-container', {
-    slidesPerView: 3,    //한번에 보여줄 슬라이드 개수
-    spaceBetween: 10,   // 슬라이드 사이 여백
-    centeredSlides: true,    // 1번 슬라이드가 가운데 보이기
+    slidesPerView: 3, //한번에 보여줄 슬라이드 개수
+    spaceBetween: 10, // 슬라이드 사이 여백
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
     loop: true,
     autoplay: {
         delay: 5000
@@ -75,7 +75,7 @@ new Swiper('.promotion .swiper-container', {
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
 let isHidePromotion = false;
-promotionToggleBtn.addEventListener('click', function (){
+promotionToggleBtn.addEventListener('click', function () {
     isHidePromotion = !isHidePromotion
     if (isHidePromotion) {
         // 비노출 처리
@@ -85,3 +85,28 @@ promotionToggleBtn.addEventListener('click', function (){
         promotionEl.classList.remove('hide');
     }
 });
+
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+    // `.toFixed()`를 통해 반환된 문자 데이터를,
+    // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+
+function floatingObject(selector, delay, size) {
+    gsap.to(selector,   // 선택자
+        random(1.5, 2.5),   // 애니메이션 동작 시간
+        {   // 옵션
+            y: size,
+            repeat: -1,
+            yoyo: true,
+            ease: Power1.easeInOut,
+            delay: random(0, delay)
+        });
+}
+
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
